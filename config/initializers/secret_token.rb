a952ebcae723c27568065435da0ebc5026cf47ca
max <mk@kernel-corp.com>
2014-10-06 12:24:55 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeamDays::Application.config.secret_key_base = '0cba6e230f1b96cecad8d3686a90951e6236e7da1b3911afbde4e568b0f816ee25f429cca597c054c6b9a41598a3169fc5c3e5463efa4c38372ea56a410309c8'
