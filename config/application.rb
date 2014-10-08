c4cbc238cba788375111c8e700853a731f24494c
max <mk@kernel-corp.com>
2014-10-08 13:26:47 +0700

    set ember root

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module TeamDays
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.ember.ember_path = "#{Rails.root}/app/assets/javascripts/partners"
   end
 end
