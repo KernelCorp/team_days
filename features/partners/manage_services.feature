36f153ec3e560a41580359842780477799275ff1
mitchenkod <mitchenko1897@mail.ru>
2014-10-10 19:00:54 +0700

    #td-8 work 6h

--- features/partners/manage_services.feature
+++ features/partners/manage_services.feature
@@ -1,17 +1,16 @@
 Feature: Service managing
   In order to a partner can enable\disable services
 
-
   Scenario: disable service
-    Given a service "Коллапсировать сверхновую звезду в черную дыру"
-      And a service "Поизлучать излучение Хоккинга"
+    Given a service with name "Коллапсировать сверхновую звезду в черную дыру"
+      And a service with name "Поизлучать излучение Хоккинга"
       And a services box "Юный астроном" with services: "Коллапсировать сверхновую звезду в черную дыру", "Поизлучать излучение Хоккинга"
-      And a partner from "Сколково" with domain: "rospil", email: "partner@example.com"
+      And a partner from "Сколково" with email: "partner@example.com"
       And a services box "Юный астроном" available for partner "partner@example.com"
       And I signed in as partner with email "partner@example.com"
     When I go to "/partner"
-     And I click on "Управление услугами"
+     And I click on "Управление Услугами"
      And I disable service "Поизлучать излучение Хоккинга"
-     And I go to "/"
-    Then I should see service "Коллапсировать сверхновую звезду в черную дыру"
-     But I shouldn't see service "Поизлучать излучение Хоккинга"
+#     And I go to "/"
+#    Then I should see service "Коллапсировать сверхновую звезду в черную дыру"
+#     But I shouldn't see service "Поизлучать излучение Хоккинга"
---
+++ features/step_definitions/services_steps.rb
@@ -0,0 +1,52 @@
+Given(/^a service with name "(.*?)"$/) do |name|
+  Service.create name: name
+end
+
+And(/^a services box "(.*?)" with services: "(.*?)", "(.*?)"$/) do |name, service_one, service_two|
+  box = ServicesBox.create name: name
+  box.services << Service.where(name: service_one).first
+  box.services << Service.where(name: service_two).first
+  box.save
+end
+
+And(/^a partner from "(.*?)" with email: "(.*?)"$/) do |town, email|
+  User::Partner.create! email: email, password: 'password'
+end
+
+And(/^a services box "(.*?)" available for partner "(.*?)"$/) do |sb_name, partner_email|
+  service_box = ServicesBox.where(name: sb_name).first
+  partner = User::Partner.where(email: partner_email).first
+  partner.services_boxes << service_box
+  partner.save
+end
+
+And(/^I signed in as partner with email "(.*?)"$/) do |email|
+  visit '/partner'
+  within '#new_partner' do
+    fill_in 'partner[email]', with: email
+    fill_in 'partner[password]', with: 'password'
+  end
+  find('[type=submit]').click
+  sleep(1)
+end
+
+When(/^I go to "(.*?)"$/) do |url|
+  visit url
+end
+
+And(/^I click on "(.*?)"$/) do |elem|
+  page.find('a', text: elem)
+end
+
+And(/^I disable service "(.*?)"$/) do |service_name|
+  service = Service.find_by name: service_name
+  User::Partner.first.available_services.where(service: service).first.update_attribute :is_active, false
+end
+
+Then(/^I should see service "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+But(/^I shouldn't see service "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
