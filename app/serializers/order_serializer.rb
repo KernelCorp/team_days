0209bc85000afebebda44a08901d678a3b126c97
max <mk@kernel-corp.com>
2014-10-13 18:45:39 +0700

    serialize service id

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,13 @@
 class OrderSerializer < ActiveModel::Serializer
   include MongoIdSerializer
 
-  attributes :id, :cost, :client_email, :client_name, :client_phone, :service_name, :status
+  attributes :id, :cost, :client_email, :client_name, :client_phone, :service_name, :status, :available_service_id
+
+  def available_service_id
+    if object.partner.present?
+      object.partner.available_services.find_by(service: object.service).id.to_s
+    else
+      nil
+    end
+  end
 end
