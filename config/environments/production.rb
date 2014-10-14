616afdcbd9e8f825b75e307f2459cb4966940bbe
max <mk@kernel-corp.com>
2014-10-14 19:26:07 +0700

    for production 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ TeamDays::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.host = 'teamdays.kerweb.ru'
+
 end
