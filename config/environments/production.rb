671c3d7efa991df03f40d0030ddd6e3e5e6beca1
max <mk@kernel-corp.com>
2014-10-14 19:23:21 +0700

    for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ TeamDays::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( partners.js partners.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
