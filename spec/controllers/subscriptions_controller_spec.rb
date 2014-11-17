7a3377a90d6348c045313a7faee58389e376587c
max <mk@kernel-corp.com>
2014-11-17 14:07:59 +0600

    fix spec

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -10,26 +10,26 @@ RSpec.describe SubscriptionsController, :type => :controller do
     let(:category) { FactoryGirl.create :category }
 
     it 'assigns new subscription as @subscription' do
-      post :create, subscription: {email: 'newemail@example.com', category_ids: [category.id]}
+      post :create, subscription: {email: 'newemail@example.com', category_attributes: {1 => {id: category.id, _destroy: 0}}}
       expect(assigns :subscription).to be_a(Subscription)
     end
 
     context 'new subscription' do
-      subject{post :create, subscription: {email: 'newemail@example.com', category_ids: [category.id]}}
+      subject{post :create, subscription: {email: 'newemail@example.com', category_attributes: {1 => {id: category.id, _destroy: 0}}}}
       it 'creates new subscription' do
         expect{subject}.to change{Subscription.count}.by(1)
       end
     end
 
     context 'existing subscription' do
-      subject{post :create, subscription: {email: subscription.email, category_ids: [category.id]}}
+      subject{post :create, subscription: {email: subscription.email, category_attributes: {1 => {id: category.id, _destroy: 0}}}}
       it 'does not create new subscription' do
         subscription
         expect{subject}.not_to change{Subscription.count}
       end
 
       it 'updates entity' do
-        expect{subject}.to change{subscription.reload.categories}.to([category])
+        expect{subject}.to change{subscription.reload.categories.count}.by(1)
       end
     end
   end
