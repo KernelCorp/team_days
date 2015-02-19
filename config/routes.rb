e54cb55d829fd9a629d2a55da61f1d26d10a7741
max-konin <mk@kernel-corp.com>
2015-02-19 16:24:45 +0600

    fix title and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 TeamDays::Application.routes.draw do
 
 
-  root "landing#show"
+  #root "landing#show"
   get "temporary/index"
- # root "temporary#index"
+  get 'landing/show'
+  root "temporary#index"
   get "main/show"
+
   resources :payments
 
   resources :orders
